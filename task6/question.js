c3ec2649695514e94aef3d7703e070883c0d0930
Payal Kumari <kumaripayalladher@gmail.com>
2024-06-27 04:17:24 -0700

    Add files via upload

---
+++ task6/question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "A. HyperText Markup Language",
+        options: [
+            "A. HyperText Markup Language",
+            "B. HomeTool Markup Language",
+            "C. Hyperlinks and Text Markup Language",
+            "D. Home Text Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following is a JavaScript data type?",
+        answer: "A. String",
+        options: [
+            "A. String",
+            "B. Paragraph",
+            "C. Document",
+            "D. Element"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How do you create a function in JavaScript?",
+        answer: "B. function myFunction()",
+        options: [
+            "A. function = myFunction()",
+            "B. function myFunction()",
+            "C. create myFunction()",
+            "D. myFunction function()"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the correct CSS syntax to change the background color of an element?",
+        answer: "A. background-color: blue;",
+        options: [
+            "A. background-color: blue;",
+            "B. color-background: blue;",
+            "C. background: blue;",
+            "D. bgcolor: blue;"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which built-in method combines the text of two strings and returns a new string?",
+        answer: "B. concat()",
+        options: [
+            "A. append()",
+            "B. concat()",
+            "C. attach()",
+            "D. join()"
+        ]
+    }
+];
